32e548ce02f362f647f78587efcf944998e6a37d
KaiBelmo <belmomohamedali@gmail.com>
2024-11-28 01:44:13 +0800

    frontend: add header

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     './src/**/*.{astro,html,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true, 
+      },
+    },
+    container: {
+      screens: {
+        sm: '600px',
+        md: '700px',
+        lg: '900px',
+        xl: '1200px',
+      },
+    },
   },
   plugins: [],
 }
